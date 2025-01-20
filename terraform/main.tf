034b1b100aeaa81a27f0290a7f56ae831e4864cd
Rufat Hajizada <hajizada.rufat@gmail.com>
2025-01-20 03:21:12 -0500

    Added terraform that deploys virtual machines from images

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,18 @@
-
 locals {
   payload            = jsondecode(file("${path.module}/payload.json"))
   talos_schematic_id = jsondecode(data.http.talos_schematic_request.response_body).id
+  control_nodes = [
+    for i in range(var.vm_config["control"].count) : {
+      name    = "${var.cluster_name}-ctrl-${i}"
+      address = cidrhost(var.cluster_node_network, var.vm_config["control"].first_hostnum + i)
+    }
+  ]
+  worker_nodes = [
+    for i in range(var.vm_config["worker"].count) : {
+      name    = "${var.cluster_name}-worker-${i}"
+      address = cidrhost(var.cluster_node_network, var.vm_config["worker"].first_hostnum + i)
+    }
+  ]
 }
 
 data "http" "talos_schematic_request" {
@@ -18,8 +29,124 @@ data "http" "talos_schematic_request" {
 resource "proxmox_virtual_environment_download_file" "talos_nocloud_image" {
   content_type = "iso"
   datastore_id = "local"
-  node_name    = var.proxmox_node
+  node_name    = var.proxmox_node_name
 
   file_name = "talos-${var.talos_version}-nocloud-amd64.img"
   url       = "https://factory.talos.dev/image/${local.talos_schematic_id}/v${var.talos_version}/nocloud-amd64.iso"
 }
+
+
+resource "proxmox_virtual_environment_vm" "control_plane" {
+  count           = var.vm_config["control"].count
+  name            = local.control_nodes[count.index].name
+  node_name       = var.proxmox_node_name
+  tags            = sort([var.cluster_name, "talos", "control", "terraform"])
+  stop_on_destroy = true
+  bios            = "ovmf"
+  machine         = "q35"
+  scsi_hardware   = "virtio-scsi-single"
+  operating_system {
+    type = "l26"
+  }
+  cpu {
+    type  = "host"
+    cores = var.vm_config["control"].cpu
+  }
+  memory {
+    dedicated = var.vm_config["control"].memory
+  }
+  vga {
+    type = "qxl"
+  }
+  network_device {
+    bridge = var.vm_config["control"].network
+  }
+  tpm_state {
+    version = "v2.0"
+  }
+  efi_disk {
+    datastore_id = var.vm_config["control"].efi_disk.datastore_id
+    file_format  = var.vm_config["control"].efi_disk.file_format
+    type         = var.vm_config["control"].efi_disk.type
+  }
+  disk {
+    datastore_id = var.vm_config["control"].disk.datastore_id
+    interface    = var.vm_config["control"].disk.interface
+    iothread     = var.vm_config["control"].disk.iothread
+    ssd          = var.vm_config["control"].disk.ssd
+    discard      = var.vm_config["control"].disk.discard
+    size         = var.vm_config["control"].disk.size
+    file_format  = var.vm_config["control"].disk.file_format
+    file_id      = proxmox_virtual_environment_download_file.talos_nocloud_image.id
+  }
+  agent {
+    enabled = true
+    trim    = true
+  }
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "${local.control_nodes[count.index].address}/24"
+        gateway = var.cluster_node_network_gateway
+      }
+    }
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "worker" {
+  count           = var.vm_config["worker"].count
+  name            = local.worker_nodes[count.index].name
+  node_name       = var.proxmox_node_name
+  tags            = sort([var.cluster_name, "talos", "worker", "terraform"])
+  stop_on_destroy = true
+  bios            = "ovmf"
+  machine         = "q35"
+  scsi_hardware   = "virtio-scsi-single"
+  operating_system {
+    type = "l26"
+  }
+  cpu {
+    type  = "host"
+    cores = var.vm_config["worker"].cpu
+  }
+  memory {
+    dedicated = var.vm_config["worker"].memory
+  }
+  vga {
+    type = "qxl"
+  }
+  network_device {
+    bridge = var.vm_config["worker"].network
+  }
+  tpm_state {
+    version = "v2.0"
+  }
+  efi_disk {
+    datastore_id = var.vm_config["worker"].efi_disk.datastore_id
+    file_format  = var.vm_config["worker"].efi_disk.file_format
+    type         = var.vm_config["worker"].efi_disk.type
+  }
+  disk {
+    datastore_id = var.vm_config["worker"].disk.datastore_id
+    interface    = var.vm_config["worker"].disk.interface
+    iothread     = var.vm_config["worker"].disk.iothread
+    ssd          = var.vm_config["worker"].disk.ssd
+    discard      = var.vm_config["worker"].disk.discard
+    size         = var.vm_config["worker"].disk.size
+    file_format  = var.vm_config["worker"].disk.file_format
+    file_id      = proxmox_virtual_environment_download_file.talos_nocloud_image.id
+  }
+  agent {
+    enabled = true
+    trim    = true
+  }
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "${local.worker_nodes[count.index].address}/24"
+        gateway = var.cluster_node_network_gateway
+      }
+    }
+  }
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "proxmox_endpoint" {
   type        = string
 }
 
-variable "proxmox_node" {
+variable "proxmox_node_name" {
   description = "Proxmox node name"
   type        = string
 }
@@ -13,3 +13,88 @@ variable "talos_version" {
   type        = string
 }
 
+variable "cluster_name" {
+  description = "Talos cluster name"
+  type        = string
+}
+
+variable "cluster_node_network_gateway" {
+  description = "The IP network gateway of the cluster nodes"
+  type        = string
+  default     = "192.168.1.1"
+}
+
+variable "cluster_node_network" {
+  description = "The IP network of the cluster nodes"
+  type        = string
+  default     = "192.168.1.1/24"
+}
+
+variable "vm_config" {
+  description = "Configuration for worker and control node VMs"
+  type = map(object({
+    count = number
+    cpu   = number
+    disk = object({
+      datastore_id = string
+      interface    = string
+      iothread     = bool
+      ssd          = bool
+      discard      = string
+      size         = number
+      file_format  = string
+    })
+    efi_disk = object({
+      datastore_id = string
+      file_format  = string
+      type         = string
+    })
+    first_hostnum = number
+    memory        = number
+    network       = string
+  }))
+  default = {
+    control = {
+      count = 1
+      cpu   = 2
+      disk = {
+        datastore_id = "local-lvm"
+        interface    = "scsi0"
+        iothread     = true
+        ssd          = true
+        discard      = "on"
+        size         = 32
+        file_format  = "raw"
+      }
+      efi_disk = {
+        datastore_id = "local-lvm"
+        file_format  = "raw"
+        type         = "4m"
+      }
+      first_hostnum = 70
+      memory        = 4096
+      network       = "vmbr0"
+    }
+    worker = {
+      count = 2
+      cpu   = 2
+      disk = {
+        datastore_id = "local-lvm"
+        interface    = "scsi0"
+        iothread     = true
+        ssd          = true
+        discard      = "on"
+        size         = 64
+        file_format  = "raw"
+      }
+      efi_disk = {
+        datastore_id = "local-lvm"
+        file_format  = "raw"
+        type         = "4m"
+      }
+      first_hostnum = 80
+      memory        = 4096
+      network       = "vmbr0"
+    }
+  }
+}
