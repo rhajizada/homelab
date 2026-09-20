59a309323e2b3d3da81aaba2919fbc00d5c0e481
Rufat Hajizada <hajizada.rufat@gmail.com>
2026-09-20 02:39:22 -0400

    ugrade kaneo to 2.25.0

--- apps/kaneo.tf
+++ apps/kaneo.tf
@@ -9,7 +9,7 @@ locals {
     }
 
     api = {
-      image = "ghcr.io/usekaneo/api:2.5.2"
+      image = "ghcr.io/usekaneo/api:2.25.0"
       resources = {
         requests = {
           cpu    = "250m"
@@ -23,7 +23,7 @@ locals {
     }
 
     web = {
-      image = "ghcr.io/usekaneo/web:2.5.2"
+      image = "ghcr.io/usekaneo/web:2.25.0"
       resources = {
         requests = {
           cpu    = "250m"
